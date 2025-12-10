4bafcac7432ffcd4d163614cdfa96931a8832f9a
Dojiawesome2000 <145794573+Dojiawesome2000@users.noreply.github.com>
2025-12-10 10:27:40 -0600

    Update AutoBlueClose.java
    Red Path and Actions added to Blue auto.
    
    Adjustments needed:
    - fix path values to mirror red path during practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoBlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoBlueClose.java
@@ -60,24 +60,55 @@ public class AutoBlueClose extends LinearOpMode{
         if (isStopRequested()) return;
 
         // change trajectory if needed here with if statements
-        TrajectoryActionBuilder ScootBackFirst = drive.actionBuilder(initialPose)
-                .lineToX(params.backwardAmount);
+//        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+//                .lineToX(params.backwardAmount);
+//
+//        Action endPath = drive.actionBuilder(new Pose2d(params.backwardAmount, 0, Math.toRadians(0)))
+//                .turnTo(params.turnAngle)
+//                .lineToX(params.initialX+5)
+//                .build();
 
-        Action endPath = drive.actionBuilder(new Pose2d(params.backwardAmount, 0, Math.toRadians(0)))
+        Action fullPath = drive.actionBuilder(initialPose)
+                .strafeTo(new Vector2d(params.initialX + params.backwardAmount, params.initialY))
+                .waitSeconds(5) // shooting
                 .turnTo(params.turnAngle)
-                .lineToX(params.initialX+5)
+                .strafeTo(new Vector2d(-(params.initialX+params.backwardAmount+10), params.initialY+10))// align with 1st layer of artifacts
+                .strafeTo(new Vector2d(-params.initialX, params.initialY+45)) // also begin intaking
+                .strafeTo(new Vector2d(-(params.initialX+params.backwardAmount-20), params.initialY+10))// scoot back and prepare to get back to shooting pos
+                .turnTo(Math.toRadians(params.returnAngleDeg))
+                .waitSeconds(5) // wait to shoot
+                .turnTo(Math.toRadians(100))
+                .strafeTo(new Vector2d(-params.initialX, params.initialY+45))
+                // then move back off line
                 .build();
 
-        Action moveBackward;
-        moveBackward = ScootBackFirst.build();
+//        Actions.runBlocking(
+//                new SequentialAction(
+//                    moveBackward,
+//                    attachments.spinUp(params.tgtRPM),
+//                    attachments.fireArtifact(5, params.tgtRPM, params.tgtShootSpeed),
+//                    endPath
+//                        // add other actions / trajectories
+//                )
+//        );
 
         Actions.runBlocking(
-                new SequentialAction(
-                        moveBackward,
-                        attachments.spinUp(params.tgtRPM),
-                        attachments.fireArtifact(5, params.tgtRPM, params.tgtShootSpeed),
-                        endPath
-                        // add other actions / trajectories
+                new ParallelAction(
+                        attachments.displayRunTime(telemetry),
+                        new ParallelAction(
+                                fullPath,
+                                new SequentialAction( // the movement
+                                        new SleepAction(3), // wait for first movement
+                                        attachments.spinUp(params.tgtRPM),
+                                        attachments.fireArtifact(5, params.tgtRPM, params.tgtShootSpeed),
+                                        new SleepAction(2), // wait for alignment to 1st set of balls
+                                        attachments.intake(1.67), // intake
+                                        new SleepAction(4), // wait for repositioning to shoot
+                                        attachments.spinUp(params.tgtRPM),
+                                        attachments.fireArtifact(5, params.tgtRPM, params.tgtShootSpeed)
+                                        // fin ... probably
+                                )
+                        )
                 )
         );
     }
