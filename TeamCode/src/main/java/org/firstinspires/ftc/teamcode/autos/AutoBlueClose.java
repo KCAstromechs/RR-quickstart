d271cca56d0fc506612d42080a8fc56ca4f3347a
Dojiawesome2000 <145794573+Dojiawesome2000@users.noreply.github.com>
2025-12-10 20:32:47 -0600

    FIxed red close auto and mirrored it to blue.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoBlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/AutoBlueClose.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -24,7 +27,11 @@ public class AutoBlueClose extends LinearOpMode{
         public double initialAngle = Math.toRadians(0);
 
         public double backwardAmount = 50;
-        public double turnAngle = Math.toRadians(219);
+        public double turnAngle = -140;
+
+        public double returnAngleDeg = -30;
+//        public double returnAngle = Math.toRadians(returnAngleDeg); // not initial angle bc goofy
+
         public double tgtRPM = 30;
         public double tgtShootSpeed = .2; // 1.0 = 100%
     }
@@ -69,16 +76,16 @@ public class AutoBlueClose extends LinearOpMode{
 //                .build();
 
         Action fullPath = drive.actionBuilder(initialPose)
-                .strafeTo(new Vector2d(params.initialX + params.backwardAmount, params.initialY))
+                .strafeTo(new Vector2d(params.initialX + params.backwardAmount, -(params.initialY)))
                 .waitSeconds(5) // shooting
-                .turnTo(params.turnAngle)
-                .strafeTo(new Vector2d(-(params.initialX+params.backwardAmount+10), params.initialY+10))// align with 1st layer of artifacts
-                .strafeTo(new Vector2d(-params.initialX, params.initialY+45)) // also begin intaking
-                .strafeTo(new Vector2d(-(params.initialX+params.backwardAmount-20), params.initialY+10))// scoot back and prepare to get back to shooting pos
+                .turnTo(Math.toRadians(params.turnAngle))
+                .strafeTo(new Vector2d(params.initialX+params.backwardAmount+10, -(params.initialY+10)))// align with 1st layer of artifacts
+                .strafeTo(new Vector2d(params.initialX, -(params.initialY+45))) // also begin intaking
+                .strafeTo(new Vector2d(params.initialX+params.backwardAmount-20, -(params.initialY+10)))// scoot back and prepare to get back to shooting pos
                 .turnTo(Math.toRadians(params.returnAngleDeg))
                 .waitSeconds(5) // wait to shoot
-                .turnTo(Math.toRadians(100))
-                .strafeTo(new Vector2d(-params.initialX, params.initialY+45))
+                .turnTo(Math.toRadians(-120))
+                .strafeTo(new Vector2d(params.initialX-20, -(params.initialY+45)))
                 // then move back off line
                 .build();
 
@@ -102,7 +109,7 @@ public class AutoBlueClose extends LinearOpMode{
                                         attachments.spinUp(params.tgtRPM),
                                         attachments.fireArtifact(5, params.tgtRPM, params.tgtShootSpeed),
                                         new SleepAction(2), // wait for alignment to 1st set of balls
-                                        attachments.intake(1.67), // intake
+                                        attachments.intake(1.7), // intake
                                         new SleepAction(4), // wait for repositioning to shoot
                                         attachments.spinUp(params.tgtRPM),
                                         attachments.fireArtifact(5, params.tgtRPM, params.tgtShootSpeed)
